14f9651f2969cc6b573933c5feecba3b684b8ef2
kizito3 <chigokizzy@gmail.com>
2023-07-27 08:59:52 -0700

    build done

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Contact1 from '../../src/component/images/contact1.png';
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="container text-center contact">
+        <div className="col-md-12">
+          <h3>Contact us</h3>
+          <p>Any question or remarks? Just write us a message!</p>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={Contact1} alt="" className='img-fluid' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
